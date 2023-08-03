0029ba6c4239d22ce717db7a92a4eb768bce2246
Johnskyle Sebastian <bestiejohn10@gmail.com>
2023-08-03 21:26:58 +0800

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResponsiveAppBar from './ResponsiveAppBar'
+
+function Home() {
+  return (
+    <div className='homeRoot'>
+        <ResponsiveAppBar />
+    </div>
+  )
+}
+
+export default Home
